6ba0e1a365d357fa0aabe0c7f26bcd5de342c41c
少轻狂 <1677568218@qq.com>
2022-05-08 21:25:53 +0800

    feat(example): head

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,15 +1,10 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, chromium } from '@playwright/test';
 
-test('my test', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-
-  // Expect an attribute "to be strictly equal" to the value.
-  await expect(page.locator('text=Get Started').first()).toHaveAttribute('href', '/docs/intro');
-
-  await page.click('text=Get Started');
-  // Expect some text to be visible on the page.
-  await expect(page.locator('text=Introduction').first()).toBeVisible();
+test('basic test',async () => {
+  const browser = await chromium.launch({ headless: false, slowMo: 50 });
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto('https://baidu.com');
+  await page.screenshot({ path: 'screenshot.png' });
+  await browser.close();
 });
