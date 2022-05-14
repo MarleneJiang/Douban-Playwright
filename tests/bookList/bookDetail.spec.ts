718bcd307a9d9b1471de3bad97f0bb1e1f9e6996
camera-2018 <2907618001@qq.com>
2022-05-14 21:51:00 +0800

    feat: 通信图书数据

--- tests/bookList/bookDetail.spec.ts
+++ tests/bookList/bookDetail.spec.ts
@@ -5,14 +5,14 @@ import { getBooks } from "./bookList.spec";
 test.describe('bookDetail', async () => {
   test('get book details', async ({ page }) => {
     let bookDetailsList = [];
-    const bookList = new ExcelService().readJsonFromExcelFile('哲学图书数据-1652459074748.xlsx');
+    const bookList = new ExcelService().readJsonFromExcelFile('通信图书数据-1652535990284.xlsx');
     for (let i = 0; i < bookList.length; i++) {
       const bookDetailUrl = bookList[i].bookUrl;
       await page.goto(bookDetailUrl);
       let bookDetails = await getBookDetails(page);
       bookDetailsList.push(bookDetails);
     }
-    new ExcelService().exportAsExcelFile(bookDetailsList, `哲学图书数据`);
+    new ExcelService().exportAsExcelFile(bookDetailsList, `通信图书数据`);
   });
 
 });
